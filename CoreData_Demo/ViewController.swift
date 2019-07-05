58d387f5ba9337af802e4177d22950f788e82d4b
NandaMochammad <nanda.mocha@gmail.com>
2019-07-05 11:55:41 +0700

    Done Happy Learning

--- CoreData_Demo/ViewController.swift
+++ CoreData_Demo/ViewController.swift
@@ -7,14 +7,68 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var namaTF: UITextField!
+    @IBOutlet weak var phoneTF: UITextField!
+    @IBOutlet weak var namaLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    
+    var data : [Contact]?
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+
+        
+        loadItems()
+
     }
 
-
+    @IBAction func addButtonTapped(_ sender: Any) {
+        
+        let contactData = Contact(context: self.context)
+        contactData.nama = namaTF.text
+        contactData.noHP = phoneTF.text
+        
+        data?.append(contactData)
+        saveData()
+    }
+    
+    func saveData() {
+        
+        do {
+            try context.save()
+        } catch  {
+            print("Error nih guys, ", error)
+        }
+        
+        updateLayout()
+    }
+    
+    func loadItems(){
+        let request : NSFetchRequest = Contact.fetchRequest()
+        
+        do {
+            data = try context.fetch(request)
+        } catch  {
+            print("Error cuy, benerin dong!, ", error)
+        }
+        
+        updateLayout()
+    }
+    
+    func updateLayout(){
+        namaLabel.text = data?.last?.nama
+        phoneLabel.text = data?.last?.noHP
+    }
+    
+    
+    
 }
 
